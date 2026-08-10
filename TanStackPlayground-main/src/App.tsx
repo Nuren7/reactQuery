bb8b347c700195a366a52680d457f3668826e48e
Nuren7 <nurenislam01@gmail.com>
2026-08-10 23:00:23 +0100

    1

--- TanStackPlayground-main/src/App.tsx
+++ TanStackPlayground-main/src/App.tsx
@@ -1,19 +1,33 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { createUser } from "./api"
-import { User } from "./types"
-import createUsersQueryOptions from "./queryOptions/createUsersQueryOptions"
-import Card from "./Card"
-import { useState } from "react"
+import {
+  useMutation,
+  useQuery,
+  useQueryClient,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import { createUser } from "./api";
+import { User } from "./types";
+import createUsersQueryOptions from "./queryOptions/createUsersQueryOptions";
+import Card from "./Card";
+import { useState } from "react";
+import { createUsersInfiniteQueryOptions } from "./queryOptions/createUsersInfiniteQueryOptions";
 
 function App() {
-  const [state, setState] = useState(true)
-  const { data: users } = useQuery(createUsersQueryOptions(undefined, { enabled: state }))
+  const [state, setState] = useState(true);
+
+  const { data, hasNextPage, isFetchingNextPage, fetchNextPage } =
+    useInfiniteQuery(
+      createUsersInfiniteQueryOptions(undefined, { enabled: state }),
+    );
+
+  const { data: users } = useQuery(
+    createUsersQueryOptions(undefined, { enabled: state }),
+  );
 
   const { mutate } = useMutation({
     mutationFn: (user: Omit<User, "_id">) => createUser(user),
-  })
+  });
 
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
   const handleCreate = async () => {
     const user = {
@@ -22,22 +36,26 @@ function App() {
       age: 27,
       createdAt: new Date().toDateString(),
       updatedAt: new Date().toDateString(),
-    }
+    };
     mutate(user, {
       onSuccess: (data) => {
         queryClient.invalidateQueries({
           queryKey: createUsersQueryOptions().queryKey,
-        })
-        console.log(data)
+        });
+        console.log(data);
       },
       onError: (error) => {
-        console.error("An error occured: ", error)
+        console.error("An error occured: ", error);
       },
       onSettled: (data, error, vars, context) => {
-        console.log({ data, error, vars, context })
+        console.log({ data, error, vars, context });
       },
-    })
-  }
+    });
+  };
+
+  const users = data?.pages.flatMap((page) => page.users);
+
+  // data.pages[pageNumber - 1]
 
   return (
     <div className="flex flex-col gap-4">
@@ -50,8 +68,15 @@ function App() {
       >
         Create New User
       </button>
+      <button
+        onClick={() => fetchNextPage()}
+        className="rounded-lg bg-green-500 p-2 hover:cursor-pointer"
+        disabled={!hasNextPage || isFetchingNextPage}
+      >
+        {isFetchingNextPage ? "Loading" : "Load More"}
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
